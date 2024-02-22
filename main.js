fd870a212352b031982da6536c6a2dbdca06071a
FernandoIE <133160275+MrLeox0@users.noreply.github.com>
2024-02-21 18:29:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function playSonido(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0 ;
+for(let contador = 0 ; contador<listaDeTeclas.length; contador++ ){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+
+        const idAudio = `#sonido_${instrumento}`;
+        console.log(idAudio);
+
+    tecla.onclick =  function(){
+        playSonido(idAudio);
+    };
+     tecla.onkeydown = function (evento){
+        if(evento.code = 'Space')
+        {tecla.classList.add('activa');}
+     }
+
+     tecla.onkeyup = function (){
+        tecla.classList.remove('activa');
+     }
+}
+
